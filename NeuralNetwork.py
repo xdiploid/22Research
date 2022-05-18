22194f57121f7063536f6e745a6f81caf1db18a1
xdiploid <artificialcheeto@gmail.com>
2022-05-17 22:11:47 -0500

    Wrote implementation up to some backpropogation.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,14 +8,86 @@ nnfs.init()
 
 class Layer_Dense:
 
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        self.biases = np.zeros((1,n_neurons))
+    def __init__(self, inputs, neurons):
+        self.weights = 0.01 * np.random.randn(inputs, neurons)
+        self.biases = np.zeros((1,neurons))
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases= np.sum(dvalues, axis=1, keepdims=True)
+        self.dinputs =  np.dot(dvalues, self.weights.T)
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.input = inputs
+        self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues): 
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
+    
+class Activation_Softmax: 
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values/ np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            jacobian_matrix = np.diagflat(single_output) - \
+                                np.dot(single_output, single_output.T)
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
+            
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2: 
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+    
+    def backward(self, dvalues, y_true):
+        samples = len(dvalues)
+        labels = len(dvalues[0])
+
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+        
+        self.dinputs = -y_true/dvalues
+        self.dinputs = self.dinputs/samples
 
 X, y = spiral_data(samples=100, classes=3)
 dense1 =  Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+loss_function = Loss_CategoricalCrossentropy()
 dense1.forward(X)
-print(dense1.output[:5])
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+print(activation2.output[:5])
+loss = loss_function.calculate(activation2.output, y)
+print('loss:', loss)
+predictions= np.argmax(activation2.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions==y)
+print('acc:', accuracy)
